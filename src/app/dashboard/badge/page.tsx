038834561f4185ff93238c1629867ebbc2c6829e
SebasMen <sebastian.meneses@antpack.com>
2024-05-16 14:44:33 -0500

    [ADD] - badge component

---
+++ src/app/dashboard/badge/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { Badge } from "@/components/ui/badge";
+
+export default function Page() {
+
+  return (
+    <div className="flex gap-4">
+      <Badge variant="default">default</Badge>
+      
+      <Badge variant="destructive">destructive</Badge>
+
+      <Badge variant="secondary">secondary</Badge>
+
+      <Badge variant="outline">outline</Badge>
+
+      <Badge capitalize variant="info">info</Badge>
+
+      <Badge capitalize variant="success">success</Badge>
+    </div>
+  );
+}
