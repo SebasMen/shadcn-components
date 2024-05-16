06dbdbf585cc018fbb885a36527874a097e3558e
SebasMen <sebastian.meneses@antpack.com>
2024-05-16 11:55:10 -0500

    [ADD] - alert component

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,8 +1,34 @@
+import { RocketIcon, CheckCircledIcon, Cross2Icon } from "@radix-ui/react-icons";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-3">
+      <Alert>
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant="destructive">
+        <Cross2Icon className="h-4 w-4" />
+        
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      
+      <Alert variant="success">
+        <CheckCircledIcon className="h-4 w-4" />
+        <AlertTitle>Success!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
